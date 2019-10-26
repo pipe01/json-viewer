ccdf2f6aceb2965807aa21e188de23f5f5444514
Felipe Martinez <pipernene@gmail.com>
2019-10-26 22:48:35 +0200

    Add toolbar

---
+++ JSON Viewer/JsonPath.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace JSON_Viewer
+{
+    public static class JsonPath
+    {
+        private static readonly Regex ArrayAccessPattern = new Regex(@"(?<=\[)\d+(?=\])");
+
+        public static JsonElement Query(this JsonElement element, string path)
+        {
+            var parts = path.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                var arrayAccess = ArrayAccessPattern.Match(parts[i]);
+
+                if (arrayAccess.Success)
+                {
+                    if (element.ValueKind == JsonValueKind.Array)
+                        element = element.EnumerateArray().ElementAt(int.Parse(arrayAccess.Value));
+                    else
+                        throw new FormatException("tried to index a non-array");
+                }
+                else
+                {
+                    var prop = element.EnumerateObject().SingleOrDefault(o => o.Name == parts[i]);
+
+                    if (prop.Name == null)
+                        throw new InvalidOperationException($"cannot find property '{parts[i]}'");
+
+                    element = prop.Value;
+                }
+            }
+
+            return element;
+        }
+    }
+}
--- JSON Viewer/MainWindow.xaml.cs
+++ JSON Viewer/MainWindow.xaml.cs
@@ -1,13 +1,14 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+﻿using Ookii.Dialogs.Wpf;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -32,19 +33,38 @@ namespace JSON_Viewer
 
         private readonly ContextMenu ItemMenu;
 
+        private JsonDocument CurrentDocument;
+
         public MainWindow()
         {
             InitializeComponent();
 
             this.DataContext = this;
             this.ItemMenu = FindResource(nameof(ItemMenu)) as ContextMenu;
+        }
 
-            var testData = new WebClient().DownloadString("https://stats2.u.gg/lol/1.1/matchups/9_21/ranked_solo_5x5/84/1.2.6.json");
+        private async void Window_Initialized(object sender, EventArgs e)
+        {
+            await Load("data.json");
+        }
 
-            var token = JToken.Parse(testData);
-            AddToken(token, Items);
+        private async Task Load(string path)
+        {
+            using var file = File.OpenRead(path);
+            await Load(file);
+        }
 
-            void AddToken(JToken t, IList items, string name = null)
+        private async Task Load(Stream data)
+        {
+            Items.Clear();
+            Items.Add(new TreeViewItem { Header = "Loading..." });
+
+            CurrentDocument = await JsonDocument.ParseAsync(data);
+            AddToken(CurrentDocument.RootElement, Items);
+
+            Items.RemoveAt(0);
+
+            void AddToken(JsonElement t, IList items, object name = null, string path = "")
             {
                 var item = new TreeViewItem();
 
@@ -68,48 +88,73 @@ namespace JSON_Viewer
 
                     loadChildren = loadChildren && item.Items.Count == 1 && item.Items[0] == null;
 
-                    switch (t)
+                    switch (t.ValueKind)
                     {
-                        case JArray arr:
-                            item.Header = $"[{arr.Count}]";
+                        case JsonValueKind.Array:
+                            var arrLen = t.GetArrayLength();
+                            item.Header = $"[{arrLen}]";
 
                             if (loadChildren)
                             {
                                 int i = 0;
-                                foreach (var arrItem in arr)
+                                foreach (var arrItem in t.EnumerateArray())
                                 {
-                                    AddToken(arrItem, item.Items, (i++).ToString());
+                                    AddToken(arrItem, item.Items, i, path + $".[{i}]");
+                                    i++;
                                 }
                             }
 
-                            hasChildren = arr.Count > 0;
+                            hasChildren = arrLen > 0;
                             break;
-                        case JObject obj:
+                        case JsonValueKind.Object:
                             item.Header = "{...}";
 
                             if (loadChildren)
                             {
-                                foreach (var objItem in obj)
+                                foreach (var objItem in t.EnumerateObject())
                                 {
-                                    AddToken(objItem.Value, item.Items, objItem.Key);
+                                    AddToken(objItem.Value, item.Items, objItem.Name, path + $".{objItem.Name}");
                                 }
                             }
 
-                            hasChildren = obj.Count > 0;
+                            hasChildren = t.EnumerateObject().MoveNext();
                             break;
-                        case JValue val:
-                            item.Header = JsonConvert.SerializeObject(val.Value);
+                        default:
+                            item.Header = JsonSerializer.Serialize(t);
                             break;
                     }
 
                     if (name != null)
-                        item.Header = $"\"{name}\":   " + item.Header;
+                        item.Header = (name is int ? name : $"\"{name}\"") + ":   " + item.Header;
 
                     if (loadChildren)
                         item.Items.RemoveAt(0);
 
+                    item.Tag = path;
                     return hasChildren;
                 }
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            string path = ((TreeViewItem)Tree.SelectedItem).Tag.ToString();
+
+            Clipboard.SetText(JsonSerializer.Serialize(CurrentDocument.RootElement.Query(path)));
+        }
+
+        private async void Open_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new VistaOpenFileDialog();
+            
+            if (dialog.ShowDialog() == true)
+            {
+                await Load(dialog.FileName);
             }
         }
     }
